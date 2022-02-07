92ed4c6b8d724b3b0e22feedf1356e631383e5c4
gabrielgrote <gabriel.grote@gmail.com>
2022-02-07 10:31:14 -0300

    added tower fuctionality

--- Main.java
+++ Main.java
@@ -7,15 +7,17 @@ class Main{
         {"-","-","-","-","-","-","-","-"},
         {"-","-","-","-","-","-","-","-"},
         {"-","-","-","-","-","-","-","-"},
+        {"-","-","-","P","T","-","-","-"},
         {"-","-","-","-","-","-","-","-"},
         {"-","-","-","-","-","-","-","-"},
-        {"-","-","-","-","-","-","-","-"},
-        {"p","p","p","p","p","p","p","p"},
-        {"T","H","B","Q","K","B","H","T"},
+        {"-","P","P","P","P","P","P","P"},
+        {"-","H","B","Q","K","B","H","T"},
     };
     public static void main(String[] args){
+        displayBoard();
         int[] input = readInput();
-        
+        verifyMove(input);
+        displayBoard();
 
     }
 
@@ -23,15 +25,23 @@ class Main{
         System.out.println("Passe a linha do tabuleiro e a coluna para selecionar peça: ");
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine().toString();
-        int lineFrom = input.charAt(0);
-        int columnFrom = input.charAt(1);
+        int lineFrom = Character.getNumericValue(input.charAt(0));
+        int columnFrom = Character.getNumericValue(input.charAt(1));
+        lineFrom = lineFrom -1;
+        columnFrom = columnFrom -1;
 
         System.out.println("Passe a linha do tabuleiro e a coluna para mover a peça: ");
         Scanner scan2 = new Scanner(System.in);
         String input2 = scan2.nextLine().toString();
-        int lineTo = input.charAt(0);
-        int columnTo = input.charAt(1);
-        
+        int lineTo = Character.getNumericValue(input2.charAt(0));
+        int columnTo = Character.getNumericValue(input2.charAt(1));
+        lineTo = lineTo -1;
+        columnTo = columnTo -1;
+
+        System.out.println(lineFrom);
+        System.out.println(columnFrom);
+        System.out.println(lineTo);
+        System.out.println(columnTo);
         return new int[] {lineFrom, columnFrom, lineTo, columnTo};
     }
 
@@ -39,6 +49,72 @@ class Main{
         for (int i=0;i<board.length;i++){
             System.out.println(Arrays.toString(board[i]));
         }
+    }
+
+    public static void verifyMove(int[] arr){
+        // pawn
+        if (board[arr[0]][arr[1]] == "P"){
+            if (arr[2] == arr[0]-1 && arr[3] == arr[1]){
+                board[arr[2]][arr[3]] = "P";
+                board[arr[0]][arr[1]] = "-";
+            }
+        }
+        // tower
+        if (board[arr[0]][arr[1]] == "T"){
+            boolean flag = false;
+            if (arr[1]<arr[3] || arr[2]<arr[0]){
+                if (arr[2] == arr[0] && arr[3] != arr[1]){
+                    for (int i = arr[1];i<arr[3];i++){
+                        if (board[arr[0]][i] != "-"){
+                            flag = true;
+                        }
+                    }
+                    if (!flag){
+                        board[arr[2]][arr[3]] = "T";
+                        board[arr[0]][arr[1]] = "-";
+                    }
+                }
+                if (arr[2] != arr[0] && arr[3] == arr[1]){
+                    for (int i = arr[2];i<arr[0];i++){
+                        if (board[i][arr[1]] != "-"){
+                            flag = true;
+                        }
+                        
+                    }
+                    if (!flag){
+                        board[arr[2]][arr[3]] = "T";
+                        board[arr[0]][arr[1]] = "-";
+                    }
+                }
+            }
+            if (arr[1]>arr[3] || arr[2]>arr[0]){
+                if (arr[2] == arr[0] && arr[3] != arr[1]){
+                    for (int i = arr[3];i<arr[1];i++){
+                        if (board[arr[0]][i] != "-"){
+                            flag = true;
+                        }
+                    }
+                    if (!flag){
+                        board[arr[2]][arr[3]] = "T";
+                        board[arr[0]][arr[1]] = "-";
+                    }
+                }
+                if (arr[2] != arr[0] && arr[3] == arr[1]){
+                    for (int i = arr[2];i<arr[0];i++){
+                        if (board[i][arr[1]] != "-"){
+                            flag = true;
+                        }
+                        
+                    }
+                    if (!flag){
+                        board[arr[2]][arr[3]] = "T";
+                        board[arr[0]][arr[1]] = "-";
+                    }
+                }
+            }
+            
+        }
+        
     }
     
 }
