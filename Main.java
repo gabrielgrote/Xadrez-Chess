be9cc73f98e6adfbe1a84ec8ca68815be92b22f2
gabrielgrote <gabriel.grote@gmail.com>
2022-02-06 22:11:22 -0300

    added more functionality to readInput

--- Main.java
+++ Main.java
@@ -14,18 +14,25 @@ class Main{
         {"T","H","B","Q","K","B","H","T"},
     };
     public static void main(String[] args){
-        String input = readInput();
-        int line = Character.getNumericValue(input.charAt(0));
-        int column = Character.getNumericValue(input.charAt(1));
-        
+        int[] input = readInput();
         
 
     }
 
-    public static String readInput(){
+    public static int[] readInput(){
+        System.out.println("Passe a linha do tabuleiro e a coluna para selecionar peça: ");
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine().toString();
-        return input;
+        int lineFrom = input.charAt(0);
+        int columnFrom = input.charAt(1);
+
+        System.out.println("Passe a linha do tabuleiro e a coluna para mover a peça: ");
+        Scanner scan2 = new Scanner(System.in);
+        String input2 = scan2.nextLine().toString();
+        int lineTo = input.charAt(0);
+        int columnTo = input.charAt(1);
+        
+        return new int[] {lineFrom, columnFrom, lineTo, columnTo};
     }
 
     public static void displayBoard(){
